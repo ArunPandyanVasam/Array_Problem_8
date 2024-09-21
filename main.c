f252b228ae4a4014fc81731c789ccd4fa22ad8a4
ArunPandyanVasam <you@example.com>
2024-09-20 21:37:24 -0400

    Semicolon

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(void) {
     //  Write a program in C to count the frequency of each element of an array.
     int n;
     int arr[SIZE];
+    int fr[SIZE];
     printf("Input the number of elements to be stored in the array: ");
     scanf("%d", &n);
     printf("Input %d elements in the array: ", n);
@@ -12,19 +13,38 @@ int main(void) {
     for (int i = 0; i < n; ++i) {
         printf("\nElement - %d: ", i);
         scanf("%d", &arr[i]);
+        fr[i] = -1;
     }
 
-    printf("The frequency of all elements of an array: \n");
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", arr[i]);
+    }
+    puts("");
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", fr[i]);
+    }
+    puts("");
 
     for (int i = 0; i < n; ++i) {
         int ctr = 1;
-        for (int j = i+1; j < n; ++j) {
-            if (arr[i] == arr[j]) {
+        for (int j = i + 1; j < n; ++j) {
+            if(arr[i] == arr[j]) {
                 ctr++;
+                fr[j] = 0;
             }
         }
-        printf("%d occurs %d times \n", arr[i], ctr);
+
+        if (fr[i] != 0) {
+            fr[i] = ctr;
+        }
     }
+
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", fr[i]);
+    }
+
+    printf("The frequency of all elements of an array :");
+
 
 
 
